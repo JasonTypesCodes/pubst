60822d1b184b5dbc5cf232f0f7d323aa7d99710e
Jason Schindler <jason@types.codes>
2018-01-04 23:08:18 -0600

    Adds support for AMD and browsers (Thanks https://github.com/umdjs/umd)

--- src/pubst.js
+++ src/pubst.js
@@ -16,72 +16,88 @@
  *  limitations under the License.
  */
 
-const store = {};
-const subscribers = {};
 
-function isNotSet(item) {
-  return item === null || typeof item === 'undefined';
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.pubst = factory();
 }
+}(typeof self !== 'undefined' ? self : this, function () {
+  const store = {};
+  const subscribers = {};
 
-function isSet(item) {
-  return !isNotSet(item);
-}
-
-function valueOrDefault(value, def) {
-  if(isNotSet(value) && typeof def !== 'undefined'){
-    return def;
+  function isNotSet(item) {
+    return item === null || typeof item === 'undefined';
   }
 
-  return value;
-}
+  function isSet(item) {
+    return !isNotSet(item);
+  }
 
-function allSubsFor(topic) {
-  return Array.isArray(subscribers[topic]) ? subscribers[topic] : [];
-}
+  function valueOrDefault(value, def) {
+    if(isNotSet(value) && typeof def !== 'undefined'){
+      return def;
+    }
 
-function scheduleCall(callback, payload, topic) {
-  setTimeout(callback, 0, payload, topic);
-}
+    return value;
+  }
 
-function publish(topic, payload) {
-  if (store[topic] !== payload) {
-    store[topic] = payload;
-    const subs = Array.isArray(subscribers[topic]) ? subscribers[topic] : [];
+  function allSubsFor(topic) {
+    return Array.isArray(subscribers[topic]) ? subscribers[topic] : [];
+  }
 
-    if (subs.length === 0) {
-      // eslint-disable-next-line no-console
-      console.warn(`WARNING:: There are no subscribers that match '${topic}'!`);
-    } else {
-      subs.forEach(sub => {
-        scheduleCall(sub, store[topic], topic);
-      });
+  function scheduleCall(callback, payload, topic) {
+    setTimeout(callback, 0, payload, topic);
+  }
+
+  function publish(topic, payload) {
+    if (store[topic] !== payload) {
+      store[topic] = payload;
+      const subs = Array.isArray(subscribers[topic]) ? subscribers[topic] : [];
+
+      if (subs.length === 0) {
+        // eslint-disable-next-line no-console
+        console.warn(`WARNING:: There are no subscribers that match '${topic}'!`);
+      } else {
+        subs.forEach(sub => {
+          scheduleCall(sub, store[topic], topic);
+        });
+      }
     }
   }
-}
 
-function subscribe(topic, callback, def) {
-  const mySub = (payload, topic) => {
-    callback(valueOrDefault(payload, def), topic);
-  };
+  function subscribe(topic, callback, def) {
+    const mySub = (payload, topic) => {
+      callback(valueOrDefault(payload, def), topic);
+    };
 
-  subscribers[topic] = allSubsFor(topic).concat(mySub);
+    subscribers[topic] = allSubsFor(topic).concat(mySub);
 
-  const current = currentVal(topic, def);
-  if (isSet(current)) {
-    scheduleCall(mySub, current, topic);
+    const current = currentVal(topic, def);
+    if (isSet(current)) {
+      scheduleCall(mySub, current, topic);
+    }
+
+    return () => {
+      subscribers[topic] = allSubsFor(topic).filter(aSub => aSub !== mySub);
+    };
   }
 
-  return () => {
-    subscribers[topic] = allSubsFor(topic).filter(aSub => aSub !== mySub);
+  function currentVal(topic, def) {
+    return valueOrDefault(store[topic], def);
+  }
+
+  return {
+    currentVal,
+    publish,
+    subscribe
   };
-}
-
-function currentVal(topic, def) {
-  return valueOrDefault(store[topic], def);
-}
-
-module.exports = {
-  currentVal,
-  publish,
-  subscribe
-};
+}));
